2083224f986652f4cf664114a44fa5529b34601e
liuliang01 <liuliang01@sogou-inc.com>
2021-10-12 15:39:31 +0800

    init google map

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="google-map"></div>
+</template>
+<script>
+import { Loader } from '@googlemaps/js-api-loader';
+export default {
+  mounted: function () {
+     // const wgs84togcj02 = coordtransform.wgs84togcj02(116.49432212686906, 39.80330583467197);
+    const loader = new Loader({
+      apiKey: 'AIzaSyDIJq4nY4cKJogPvuBsnfb_V_Ov5MgLJQQ', // 个人申请key 按次计费
+      version: 'weekly',
+      libraries: ['places', 'drawing'],
+    });
+    // google map配置项
+    const mapOptions = {
+      center: { lat: 39.79334259095674, lng: 116.51912734023836 },
+      zoom: 18,
+      mapTypeControlOptions: {
+        // style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        mapTypeIds: ['roadmap', 'satellite'],
+      },
+      // mapTypeId: 'coordinate',
+      disableDefaultUI: true, // 禁用按钮
+      zoomControl: true,
+    };
+    // Promise
+    loader
+      .load()
+      .then(() => {
+        const map = new window.google.maps.Map(document.getElementById('google-map'), mapOptions);
+      })
+      .catch((e) => {
+        // do something
+      });
+  },
+}
+</script>
+<style lang="scss" scoped>
+#google-map {
+  height: 100%;
+  width: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <div>这里写页面布局</div>
+    <GoogleMap/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import GoogleMap from '@/components/GoogleMap.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    GoogleMap
   }
 }
 </script>
+<style scoped lang="scss">
+.home{
+  width: 100%;
+  height: 90%;
+}
+</style>
